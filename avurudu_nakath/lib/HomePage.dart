3b77ed07438f4814adc1da744c66c03d13fbd1e8
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-16 16:09:13 +0530

    add image

--- avurudu_nakath/lib/HomePage.dart
+++ avurudu_nakath/lib/HomePage.dart
@@ -10,6 +10,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Color(0xffFFBE45));
+    return Scaffold(
+      backgroundColor: const Color(0xffFFBE45),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            SizedBox(
+              width: 300,
+              height: 300,
+              child: Image.asset('assets/sun.png', fit: BoxFit.cover),
+            ),
+            const Text(
+              'Avurudu',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

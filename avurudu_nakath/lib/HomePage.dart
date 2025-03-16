d4cf649deb4874a6f106cfb04e4f8b4629ce8011
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-16 18:26:39 +0530

    create new container

--- avurudu_nakath/lib/HomePage.dart
+++ avurudu_nakath/lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,14 +11,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xffFFBE45),
+    return Scaffold(
+      backgroundColor: const Color(0xffFFBE45),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 70, 10, 10),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(10, 70, 10, 20),
               child: Text(
                 'සුභ අළුත් අවුරුද්දක් වේවා',
                 style: TextStyle(
@@ -25,6 +26,49 @@ class _HomePageState extends State<HomePage> {
                   fontWeight: FontWeight.w500,
                   color: Colors.black,
                   fontFamily: 'UNDisapamok',
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                height: 350,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'මීළඟ නැකත: ආහාර පිසීම',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                          fontFamily: 'UNIndeewaree',
+                        ),
+                      ),
+
+                      Row(
+                        children: [
+                          Text(
+                            'දින:',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                              fontFamily: 'UNIndeewaree',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
